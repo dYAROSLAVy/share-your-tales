2022c0b12276fbc25085ff67312dedbd5171b755
dYAROSLAVy <yadsp@yandex.ru>
2024-06-11 12:33:53 +0300

    feat: add posts filtration

--- src/screens/main-screen/ui/main-screen.tsx
+++ src/screens/main-screen/ui/main-screen.tsx
@@ -4,15 +4,37 @@ import { PostsLayout, SortTabs, useGetPosts } from "@entities/posts";
 import { PostsList } from "@widgets/posts-list";
 import { useUserMe } from "@entities/user";
 import { PostFilterType } from "@shared/apollo";
+import { useState } from "react";
 
 export const MainScreen = ({ navigation }) => {
-  const type = PostFilterType.Top;
+  const TABS = [
+    {
+      text: "New",
+      isLeft: true,
+      id: "NEW",
+    },
+    {
+      text: "Top",
+      isRight: true,
+      id: "TOP",
+    },
+  ];
+
+  const [selectedTab, setSelectedTab] = useState(TABS[0].id);
+
+  const type = selectedTab as PostFilterType;
 
   const { loading, error, data } = useGetPosts({
     variables: { type },
   });
 
   const { data: userData } = useUserMe();
+
+  const selectedTabIndex = TABS.findIndex((tab) => tab.id === selectedTab);
+
+  const createTabClickHandler = (id: string) => () => {
+    setSelectedTab(id);
+  };
 
   return (
     <PostsLayout>
@@ -26,7 +48,11 @@ export const MainScreen = ({ navigation }) => {
           }!`}
           navigation={navigation}
         />
-        <SortTabs />
+        <SortTabs
+          tabs={TABS}
+          selectedTabIndex={selectedTabIndex}
+          createTabClickHandler={createTabClickHandler}
+        />
       </View>
       <PostsList posts={data?.posts.data} />
     </PostsLayout>
